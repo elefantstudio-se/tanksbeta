915f0142c86e787c7188c15c091dae1b4e11803d
NullProgrammer <NullProgrammer@8da4169d-9a49-0410-baa0-7d6264f9b901>
2008-08-06 02:40:50 +0000

    Changed tons of code to fit the new bullet system.

--- trunk/Tanks/Tanks/Tank.cs
+++ trunk/Tanks/Tanks/Tank.cs
@@ -26,7 +26,8 @@ namespace Tanks
         public float bodyAngle;
         public float speed;
         Keys up, down, right, left;
-        Bullet bullet = new Bullet();
+
+        List<Bullet> Bullets = new List<Bullet>();
 
 
         public Tank(Vector2 cordinate, Color temp_color, Texture2D loaddedBodyTexture, Texture2D loaddedCannonTexture, Keys up, Keys down, Keys right, Keys left)
@@ -40,18 +41,31 @@ namespace Tanks
             cannonOrigin = new Vector2(cannonTexture.Width / 2, cannonTexture.Height / 2);
             color = temp_color;
             speed = 3;
+
             this.up = up;
             this.down = down;
             this.right = right;
             this.left = left;
-
-            bullet.bullet_image = Game1.bullet;
-            
-         
         }
 
-        public void move()
+        public void Fire()
         {
+            Vector2 velocity = new Vector2( (float)Math.Cos(this.cannonAngle), (float)Math.Sin(this.cannonAngle)) * 5.0f;
+            Vector2 position = new Vector2(cordinate.X - 4, cordinate.Y - 4);
+            
+            Bullet tmp = new Bullet((float)this.cannonAngle, position, velocity);
+
+            this.Bullets.Add( tmp );
+        }
+
+        public void Move()
+        {
+            MouseState mouseState = Mouse.GetState();
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                this.Fire();
+            }
 
             KeyboardState keyState = Keyboard.GetState();
 
@@ -104,7 +118,11 @@ namespace Tanks
             }
 
             update_cannon_angle();
-            bullet.update_bullet(this.cannonAngle , this.cordinate);
+
+            foreach (Bullet bullet in this.Bullets)
+            {
+                bullet.Update();
+            }
         }
 
         void update_cannon_angle()
@@ -133,20 +151,24 @@ namespace Tanks
 
 
 
-        public void draw()
+        public void Draw()
         {
             Game1.spriteBatch.Draw(bodyTexture, cordinate, 
                 null, color, 
                 bodyAngle * 0.0174444444444f, 
                 bodyOrigin, 1.0f, 
                 SpriteEffects.None, 0.0f);
-            bullet.draw_bullet();
          
             Game1.spriteBatch.Draw(cannonTexture, cordinate , 
                 null, color, 
                 (float)(cannonAngle + 1.570795), 
                 cannonOrigin, 1.0f, 
                 SpriteEffects.None, 0.0f);
+
+            foreach (Bullet bullet in this.Bullets)
+            {
+                bullet.Draw();
+            }
         }
     }
 }
